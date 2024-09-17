2c2ba9848d0d212b8f76e1035acc03651574ed3c
Muhammad Moosa <muhammadmoosa332211@gmail.com>
2024-09-17 15:55:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,152 @@
+// Java Script Class 1 (chap 1)
+// alert("Are You Ready");
+
+
+
+
+
+
+// variable for string (chap 2)
+// a = muhammad;
+// b = moosa;
+// c = a + b; 
+// console.log(c);
+
+
+
+
+// // variable for number (chap 3)
+// var a = 40;
+// let c = a * 2;
+// console.log (c)
+
+// var weight = 150;
+// weight = weight + 25; 
+// console.log (weight + 25)
+
+
+
+
+
+
+// Variable Names Legal and Illegal (chap 4)
+// legal:
+// myname = "moosa"
+// my_name = "moosa"
+// _myname = "moosa"
+// MyName = "moosa"
+// MYNAME = "moosa"
+// myname2 = "moosa"
+// myname$ = "moosa"
+// illegal:
+// 2myname = "moosa"
+// my-name = "moosa"
+// my name = "moosa"
+// my@name = "moosa"
+// my%name = "moosa"
+
+
+
+
+
+
+// Math expressions:Familiar operators (chap 5)
+// var a = 2 
+// var b = 44
+// console.log(a-b)
+// var a = 2 
+// var b = 44
+// console.log(a+b)
+// var a = 2 
+// var b = 44
+// console.log(a*b)
+// var a = 2 
+// var b = 44
+// console.log(a/b)
+
+
+
+
+
+
+
+
+// Math expressions:unfamiliar operators (chap 6)
+// var a = 2 
+// var b = 44
+// console.log(a -- b)
+// var a = 2 
+// var b = 44
+// console.log(a -+ b)
+// var a = 2 
+// var b = 44
+// console.log(a ++ b)
+
+
+
+
+
+
+//Math expressions:Eliminating ambiguity (chap 7)
+// var a = 2 
+// var b = 44
+// console.log(1 * a + b)
+// var a = 2 
+// var b = 44
+// console.log(10 * a + b)
+
+
+
+
+
+
+
+// Concatenating text strings (chap 8)
+// var name = "Moosa";
+// alert("Thanks, " + name  + "!")
+
+
+
+
+
+
+
+// //Promt (Chap 9)
+// var question = "Your Name?";
+// var spec = prompt(question)
+
+
+
+
+// Simple Calculator Using Promt
+
+// var msg1 = prompt("Enter your 1st Number");
+// var msg3 = Number(msg1);
+// var msg2 = prompt("Enter your 2nd Number");
+// var msg4 = Number(msg2);
+// // alert(msg3+msg4);
+// alert("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
